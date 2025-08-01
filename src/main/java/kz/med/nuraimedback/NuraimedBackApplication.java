4485f9dfc578be92116ed50276ae01a4ec633340
Olzhas <ola.zhenisbek@gmail.com>
2025-08-01 12:09:44 +0500

    Initialize project

---
+++ src/main/java/kz/med/nuraimedback/NuraimedBackApplication.java
@@ -0,0 +1,13 @@
+package kz.med.nuraimedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuraimedBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuraimedBackApplication.class, args);
+    }
+
+}
